cf54c07ea6f6836a7e4d8407af68541725121ce1
Hoàng Nhi <hoangnhi.120900@gmail.com>
2021-09-22 16:27:22 +0700

    commit register

--- src/main/java/com/example/spring_boot_jwt_nthn/service/UserService.java
+++ src/main/java/com/example/spring_boot_jwt_nthn/service/UserService.java
@@ -1,4 +1,12 @@
 package com.example.spring_boot_jwt_nthn.service;
 
+import com.example.spring_boot_jwt_nthn.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipal;
+
+
 public interface UserService {
+    User createUser(User user);
+
 }
