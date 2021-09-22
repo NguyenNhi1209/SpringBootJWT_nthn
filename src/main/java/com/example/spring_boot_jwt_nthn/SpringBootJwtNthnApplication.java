0399b9453bad30f43c9e56ee29e41906a7c6c1ff
Hoàng Nhi <hoangnhi.120900@gmail.com>
2021-09-22 10:38:36 +0700

    first commit

---
+++ src/main/java/com/example/spring_boot_jwt_nthn/SpringBootJwtNthnApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_nthn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtNthnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtNthnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_boot_jwt_nthn/SpringBootJwtNthnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_nthn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtNthnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
